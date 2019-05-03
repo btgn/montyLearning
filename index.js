2aa049eba5c08adfbc98dd3a82aa518d78c200e8
t0b00yv <t0b00yv@homeoffice.wal-mart.com>
2019-05-03 18:28:13 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// let, var, const
+/* let i = 3;
+var i = 5;
+const i = 10; */
+
+// - single line comment
+
+/*
+Multi line
+comment
+*/
+
+// arrays
+// const arr = ['Monty', 'Misha', 'Trilok', 'meEnaxi', 'varun', 'anuSha'];
+// for (let index = 0; index < arr.length; index++) {
+//   if (arr[index].toLowerCase() === 'meenaxi') {
+//     console.log('Found meenaxi at index: ' + index);
+//   } else {
+//     console.log('didnt find any');
+//   }
+// }
+
+// const name = ['TrilOk', 'MoNty']
+// for (let index = 0; index < name.length; index++) {
+//     console.log(name[index].toLowerCase());
+// }
